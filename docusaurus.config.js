fc757cf8929b4c8ae6916b4b85fae1f23d0e4797
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2021-08-08 16:34:39 -0300

    Remoção de título.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,7 +3,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'Cantim do Talim',
+  title: '',
   tagline: '',
   url: 'https://talim.dev',
   baseUrl: '/',
